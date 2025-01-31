d1b46fcd386e2f8c9e98ddfae7863a095d3dcf5f
Renju <84337556+R3NJU@users.noreply.github.com>
2025-01-30 22:55:18 -0500

    Testing custom docker network

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,6 +67,7 @@ resource "aws_instance" "flask_app" {
                                 sudo usermod -aG docker ec2-user
                                 newgrp docker
                                 sudo chmod 666 /var/run/docker.sock
+                                sudo docker network create -d bridge --subnet 10.0.0.0/24 --gateway 10.0.0.1 my-nw
                                 EOF
   tags = {
       "Name" = "Flask_App"
